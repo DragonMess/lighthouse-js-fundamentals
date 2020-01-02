3f59a6ad162d687685327a7df9a23d2323708f24
DragonMess <kmylop@hotmail.com>
2020-01-02 07:25:57 +0000

    ingredients loops

--- packing-list.js
+++ packing-list.js
@@ -5,3 +5,13 @@ for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
 console.log(packingList[3]);
+
+//-------------------------------
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
+
